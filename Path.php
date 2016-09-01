244739d43c669b724472e87350a374245e2f556a
Yokota Kenji <mail@yokotakenji.me>
2016-09-01 16:55:22 +0900

    first

---
+++ Path.php
@@ -0,0 +1,69 @@
+<?php
+namespace withpro\func;
+/** ////////////////////////////////////////////////////////////////////////////
+ * USEFUL PATH CLASS
+ *
+ * @author yokotak0527 <mail@yokotakenji.me>
+ */
+class Path{
+	private static function encode_special_text($path){
+		$path = str_replace(':/','{{ptcl_sep}}',$path);
+		return $path;
+	}
+	private static function decode_special_text($path){
+		$path = str_replace('{{ptcl_sep}}',':/',$path);
+		return $path;
+	}
+	/** ========================================================================
+	 * join path
+	 *
+	 * join('/a/', '/b/')     -> /a/b/
+	 * join('/a/b/', '../c/') -> /a/c/
+	 *
+	 * @param  string $arguments
+	 * @return string
+	 */
+	public static function join(){
+		$opt  = [
+			'last_slash' => 'true'
+		];
+		$args = func_get_args();
+		if(count($args) == 1 && empty($args[0])) return $args[0];
+		// オプション変更
+		if(is_array(end($args))) $opt = array_merge($opt,array_pop($args));
+		// 「/」は除外
+		$args = array_map(function($val){ return $val == '/' ? '' : $val; },$args);
+		// 空の値は削除
+		$args = array_values(array_filter($args,function($v){ return !empty($v); }));
+		// ---------------------------------------------------------------------
+		$path = trim($args[0]);
+		// 先頭
+		if(preg_match('/^\/{2}/',$path)) $path = preg_replace('/^\/{2}/','//',$path);
+		else $path = preg_replace('/^\/+/','/',$path);
+		// 末尾
+		if(preg_match('/[a-zA-Z]:\/{2}$/',$path)) $path = preg_replace('/\/+$/','/',$path);
+		else $path = preg_replace('/\/+$/','',$path);
+
+		$path = Path::encode_special_text($path);
+		$i = 1;
+		$l = count($args);
+		for(; $i<$l; $i++){
+			$base_arr = explode( '/', $path );
+			$path_arr = explode( '/', preg_replace( '/^\/+|\/+$/', '', trim($args[$i]) ) );
+			$ii = 0;
+			$ll = count($path_arr);
+			for(; $ii<$ll; $ii++){
+				if(preg_match('/^\.{2}$/',$path_arr[$ii])) array_pop($base_arr);
+				else if(preg_match('/^\.$/',$path_arr[$ii])) continue;
+				else $base_arr[] = $path_arr[$ii];
+			}
+			$path = implode('/',$base_arr);
+		}
+		$path = Path::decode_special_text($path);
+		// $path = $path == '/' ? '' : $path;
+		if($opt['last_slash'] && !preg_match('/\.[a-zA-Z]{2,4}\??[a-zA-Z0-9\-=_\.\[\]\~\+\-\\|\¥\)\(\#\!\'\"\}\{\@\$%&\^`\*\;\:]*$/',$path)){
+			$path .= '/';
+		}
+		return $path;
+	}
+}
